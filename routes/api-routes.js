a372638ce9220e5ac9bd3c74dd5258645785d41b
Andrew Lipsey <adlipsey@gmail.com>
2017-12-14 14:52:50 -0500

    onClick triggers api calls for trivia questions

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,9 @@
+
 var db = require("../models");
+var request = require("request");
+
+module.exports = function(app){
+
 
   app.post("/api/users", function(req, res) {
     db.Users.create(req.body).then(function(dbUser) {
@@ -6,6 +11,7 @@ var db = require("../models");
       res.json(dbUser);
     });
   });
+
 
   app.post("/register", function(req, res) {
     console.log(req.body);
@@ -61,3 +67,45 @@ var db = require("../models");
 //   });
 //
 // };
+
+	//API call to get questions
+	app.post("/api/qstn", function(req, res){
+		// console.log(req.body);
+		getQs(req, function(data){
+			res.json({questions: data});
+		});
+	});
+
+	function getQs(req, cb){
+		var url = "https://opentdb.com/api.php?amount="+ req.body.qspr;
+		var game = [];
+		var roundCat;
+		var i = 0
+		for(; i < req.body.rs; i++){
+			// console.log(Date.now(), i)
+			roundCat = req.body['r'+ i + 'cat'];
+			if(roundCat){
+				url += "&category="+roundCat
+				request(url, function(err, resp, body){
+					if(err) console.log(err);
+					body = JSON.parse(body);
+					game.push(body.results);
+					// console.log(game);
+					// console.log("-----")
+				});
+					// console.log("========")
+
+			}
+			else{
+				request(url, function(err, resp, body){
+					body = JSON.parse(body);
+					game.concat(body.results);
+				});
+			}
+		}
+		setTimeout(function(){
+			cb(game)
+		},i * 1000)
+	}
+};
+
