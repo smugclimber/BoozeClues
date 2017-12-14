b5d70d4ac0da8d780e23b87d90b331cb267d3d19
Andrew Lipsey <adlipsey@gmail.com>
2017-12-14 09:58:58 -0500

    Aesthetic nonsense

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,18 @@
 
   socket.on('countdown', function(time){
   	$("#sqr").text(time.left);
+  	if(time.left <=20){
+  		$("#sqr").css("background-color", "green");
+  	}
+  	if(time.left <= 10 ){
+  		$("#sqr").css("background-color", "gold");
+  	}
+  	if(time.left <=5 ){
+  		$("#sqr").css("background-color", "red");	
+  	}
+  	if(time.left === 0){
+  		$("#sqr").text("Time's Up!");
+  	}
   });
 
   $("#sqr").on("click", function(){
