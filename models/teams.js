fd9c2238cbba291b6bb98964be3efbdae178b0af
oleivahn <oleivahn@gmail.com>
2017-12-15 10:56:34 -0500

    yee

---
+++ models/teams.js
@@ -0,0 +1,25 @@
+module.exports = function(sequelize, DataTypes){
+	var Team = sequelize.define("Team", {
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		users: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		score: {
+			type: DataTypes.INTEGER,
+			defaultValue: 0
+		}
+	});
+
+	Team.associate = function(models) {
+    Team.belongsTo(models.games, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+	return Team;
+}
