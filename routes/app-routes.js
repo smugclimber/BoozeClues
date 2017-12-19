ab80ade700b850d626f27a725933e690d22eb7b6
unknown <smugclimber@icloud.com>
2017-12-19 11:37:16 -0500

    slash and burning of models...trimmed the fat!

--- routes/app-routes.js
+++ routes/app-routes.js
@@ -8,12 +8,9 @@ var router = express.Router();
       });
 
     router.post("/user", function(req, res) {
-      var name  =req.body.name;
       var GameId =req.body.GameId;
-  
-    req.checkBody('name','Name is required').notEmpty();
+
     req.checkBody('GameId','GameId is required').notEmpty();
-    req.body.userId = 1
 
     var errors = req.validationErrors();
     if(errors.length > 0){
@@ -21,38 +18,52 @@ var router = express.Router();
       res.render('user-game',{
         errors:errors
       })
- 
+
       }else {
-       db.Team.create(req.body).then(function(team) {
-        if(!team) throw "error";
-           db.User.findById(req.body.userId).then(function(user){
-            user.setTeams([team]).then(function(){
-                res.redirect("/usergame/"+GameId);
+
+        router.put("/usergame", function(req, res) {
+          db.User.update(
+            req.body,
+            {
+              where: {
+                GameId: req.body.GameId
+              }
+            }).then(function(dbPost) {
+              res.redirect("/usergame"+GameId);
             });
-          })
-      
         });
       }
-    });
+});
 
-    router.post("/game", function(req, res) {
-         var name = req.body.name;
-         // Validation
-         req.checkBody('name','Name is required').notEmpty();
-         var errors = req.validationErrors();
-         if(errors.length > 0){
-           console.log('there was an error');
-           res.render('bar-dashboard',{
-             errors:errors
-           });
-         }
-         else {
-            db.Game.create(req.body).then(function(game, err) {
-            		if(err) throw err;
-              console.log("game id: "+ game.id);
-              res.json(game);
-             });
-         }
- 	});
+    //    db.Game.add(req.body).then(function(team) {
+    //     if(!team) throw "error";
+    //        db.User.findById(req.body.userId).then(function(user){
+    //         user.setTeams([team]).then(function(){
+    //             res.redirect("/usergame/"+GameId);
+    //         });
+    //       })
+    //
+    //     });
+    //   }
+    // });
+
+    // router.post("/game", function(req, res) {
+    //      var name = req.body.name;
+    //      // Validation
+    //      req.checkBody('name','Name is required').notEmpty();
+    //      var errors = req.validationErrors();
+    //      if(errors.length > 0){
+    //        console.log('there was an error');
+    //        res.render('bar-dashboard',{
+    //          errors:errors
+    //        });
+    //      }
+    //      else {
+    //         db.Game.create(req.body).then(function(game, err) {
+    //         		if(err) throw err;
+    //           console.log("game id: "+ game.id);
+    //           res.json(game);
+    //          });
+    //      }
 
 module.exports = router;
