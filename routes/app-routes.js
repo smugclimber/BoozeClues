1897a64829331d9477ae9d0db1f3929ee5fac337
fitsum belayneh <fitsu_alt@yahoo.com>
2017-12-19 07:45:51 -0500

    teamusers association is created

--- routes/app-routes.js
+++ routes/app-routes.js
@@ -1,16 +1,4 @@
-// export.isAuthenticated = function(req, res, next){
-//   if(req.isAuthenticated()){
-//     next();
-//   }else{
-//     next(new Error(401));
-//   }
-// }
 
-// exports.destroySession = function(req, res, next) {
-//   req.logOut();
-//   req.session.destroy()
-//   res.redirect("/")
-// }
 var db = require("../models");
 var express = require('express');
 var router = express.Router();
@@ -18,48 +6,33 @@ var router = express.Router();
     router.get("/user", function(req, res) {
       res.render("user");
       });
-      // // Login
-      // router.get("/login", function(req, res) {
-      //   res.render("login");
-      //   });
-        // Register user
+
     router.post("/user", function(req, res) {
       var name  =req.body.name;
       var GameId =req.body.GameId;
-      // var password2 = req.body.password2;
-      // console.log(req)
-      // console.log(req.body)
-
-    // Validation
-
+  
     req.checkBody('name','Name is required').notEmpty();
     req.checkBody('GameId','GameId is required').notEmpty();
+    req.body.userId = 1
+
     var errors = req.validationErrors();
     if(errors.length > 0){
       console.log('===============there was an error======>')
       res.render('user-game',{
         errors:errors
       })
-      // req.flash('success_msg', 'You are registered and can now login')
+ 
       }else {
-       db.Team.create(req.body).then(function(err,team) {
-        //if(err) throw err;
-        console.log("teamname and id:"+ team);
-         res.redirect("/usergame");
+       db.Team.create(req.body).then(function(team) {
+        if(!team) throw "error";
+           db.User.findById(req.body.userId).then(function(user){
+            user.setTeams([team]).then(function(){
+                res.redirect("/usergame");
+            });
+          })
+      
         });
       }
     });
-
-
-
-        // router.post('/user',
-        //   // passport.authenticate('local', {successRedirect:'/bargame', failureRedirect:'/user', failureFlash: true}),
-        //   function(req, res){
-        //     req.flash('success_msg', "Login Successful")
-        //     res.redirect('/');
-        //   });
-
-
-
 
 module.exports = router;
